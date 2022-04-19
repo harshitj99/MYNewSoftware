a205c6533b8901753010a2da7187eab5df0951de
hj007 <harshitjain137@gmail.com>
2022-04-19 16:29:23 +0530

    added new Functionality

---
+++ func.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class func{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+System.out.println("Given no. Multiplied by 2 gives = " + 3*n);
+
+}
+}
